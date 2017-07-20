f58a000de7b4f8851d3513b4800ba632751d7f2f
Bijesh Kawan <kawanbijesh@gmail.com>
2017-07-20 08:52:44 +0545

    added folder

---
+++ whereto /myScript.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bijeshkawan on 6/20/17.
+ */
+function myFunction(){
+    document.getElementById("demo").innerHTML="My name is Bijesh Kawan";
+}
+
+function myFunctionWithVariable(firstName, lastname){
+
+    console.log(firstName);
+    document.getElementById("info").innerHTML=firstName + lastname;
+}
+
+
+def sum(data):
+    data = data.split(" ")
+    return int(data[0])+int(data[1])
+
+
+def sum(data):
+...     data = data.split(" ")
+    ...     return int(data[0]) + int(data[1])
